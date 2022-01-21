118e66475431248afff5e3d3a887991ea12c0e4a
CamilaBorges <camilakontakt@gmail.com>
2022-01-21 15:57:25 +0100

    added  js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "./Search";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather React</h1>
+      <Search />
+    </div>
+  );
+}
